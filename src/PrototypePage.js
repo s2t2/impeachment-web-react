f075cfb2181844d9116322b24d255dec111d5595
MJ Rossetti <s2t2@users.noreply.github.com>
2020-09-16 14:45:23 -0400

    Blank slate

---
+++ src/PrototypePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function PrototypePage() {
+  return (
+    <div className="prototype-page">
+      <header className="prototype-header">
+        <h1>Hello This is a Prototype</h1>
+
+      </header>
+    </div>
+  );
+}
+
+export default PrototypePage;
