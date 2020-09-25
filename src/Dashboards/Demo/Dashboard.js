220c50c1e54ac466dee02a441e9e0889fb52e74e
MJ Rossetti <s2t2@users.noreply.github.com>
2020-09-25 14:17:54 -0400

    Basic bar chart

--- src/Dashboards/Demo/Dashboard.js
+++ src/Dashboards/Demo/Dashboard.js
@@ -5,20 +5,29 @@ import Plot from 'react-plotly.js';
 
 class MyChart extends React.Component {
   render() {
+    //const layout = {width: 320, height: 240, title: 'A Fancy Plot'};
+    //var data = [
+    //    {
+    //      x: [1, 2, 3],
+    //      y: [2, 6, 3],
+    //      type: 'scatter',
+    //      mode: 'lines+markers',
+    //      marker: {color: 'red'},
+    //    },
+    //    {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+    //  ]
+
+    var layout = {title: 'My Chart'} // width: 320, height: 240,
+
+    var data = [{
+      type: 'bar',
+      x: [20, 14, 23],
+      y: ['giraffes', 'orangutans', 'monkeys'],
+      orientation: 'h'
+    }];
+
     return (
-      <Plot
-        data={[
-          {
-            x: [1, 2, 3],
-            y: [2, 6, 3],
-            type: 'scatter',
-            mode: 'lines+markers',
-            marker: {color: 'red'},
-          },
-          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-        ]}
-        layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
-      />
+      <Plot data={data} layout={layout}/>
     );
   }
 }
@@ -26,9 +35,7 @@ class MyChart extends React.Component {
 function Dashboard() {
   return (
     <div className="Dashboard">
-
       <MyChart/>
-
     </div>
   );
 }
