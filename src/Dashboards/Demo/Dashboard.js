15394deb37dcd386c7dd51c3bea1b1a5f82c74e7
MJ Rossetti <s2t2@users.noreply.github.com>
2020-09-27 10:15:54 -0400

    Display spinner until charts load

--- src/Dashboards/Demo/Dashboard.js
+++ src/Dashboards/Demo/Dashboard.js
@@ -11,31 +11,57 @@ class Dashboard extends React.Component {
   }
 
   render() {
-    var layout0 = {width: 400, height: 300, title: 'Users Most Retweeted by Community 0'}
-    var layout1 = {width: 400, height: 300, title: 'Users Most Retweeted by Community 1'} // TODO: red
-    var data0 = [{
-      type: 'bar',
-      x: [10, 20, 30],
-      y: ['User A', 'User B', 'User C'],
-      orientation: 'h'
-    }];
-    var data1 = [{
-      type: 'bar',
-      x: [5, 10, 50],
-      y: ['User X', 'User Y', 'User Z'],
-      orientation: 'h'
-    }];
+    //var layout0 = {width: 400, height: 300, title: 'Users Most Retweeted by Community 0'}
+    //var layout1 = {width: 400, height: 300, title: 'Users Most Retweeted by Community 1'} // TODO: red
+    //var data0 = [{
+    //  type: 'bar',
+    //  x: [10, 20, 30],
+    //  y: ['User A', 'User B', 'User C'],
+    //  orientation: 'h'
+    //}];
+    //var data1 = [{
+    //  type: 'bar',
+    //  x: [5, 10, 50],
+    //  y: ['User X', 'User Y', 'User Z'],
+    //  orientation: 'h'
+    //}];
 
-    console.log("ACCESSING FROM RENDER...", this.state.parsedResponse)
+    //console.log("ACCESSING FROM RENDER...", this.state.parsedResponse)
+
+    var spinIntoCharts;
+    if (this.state.parsedResponse) {
+
+        var layout0 = {width: 400, height: 300, title: 'Users Most Retweeted by Community 0'}
+        var layout1 = {width: 400, height: 300, title: 'Users Most Retweeted by Community 1'} // TODO: red
+
+        var response0 = this.state.parsedResponse.filter(function(u){ return u["community_id"] === 0})
+        var response1 = this.state.parsedResponse.filter(function(u){ return u["community_id"] === 1})
+
+        var data0 = [{
+          type: 'bar',
+          x: [10, 20, 30],
+          y: ['User A', 'User B', 'User C'],
+          orientation: 'h'
+        }];
+        var data1 = [{
+          type: 'bar',
+          x: [5, 10, 50],
+          y: ['User X', 'User Y', 'User Z'],
+          orientation: 'h'
+        }];
+
+        spinIntoCharts = <span className="chart-row">
+          <Plot className="Community-0-Chart" data={data0} layout={layout0}/>
+          <Plot className="Community-1-Chart" data={data1} layout={layout1}/>
+        </span>
+    } else {
+        spinIntoCharts = <Spinner animation="grow" />
+    };
 
     return (
        <Container className="Dashboard">
         <h3>Demo Dashboard</h3>
-
-        { this.state.parsedResponse ? null : <Spinner animation="grow" /> }
-
-        <Plot className="Community-0-Chart" data={data0} layout={layout0}/>
-        <Plot className="Community-1-Chart" data={data1} layout={layout1}/>
+        {spinIntoCharts}
       </Container>
     );
   }
@@ -43,7 +69,7 @@ class Dashboard extends React.Component {
   componentDidMount(){
     // https://reactjs.org/docs/react-component.html#componentdidmount
     // invoked immediately after a component is mounted (inserted into the tree). Initialization that requires DOM nodes should go here. If you need to load data from a remote endpoint, this is a good place to instantiate the network request.
-    console.log("[DASHBOARD DID MOUNT]")
+    console.log("DASHBOARD DID MOUNT")
     this.fetchData()
   }
 
@@ -56,7 +82,7 @@ class Dashboard extends React.Component {
     //  this.fetchData(this.props.userID);
     //}
 
-    console.log("[DASHBOARD DID UPDATE]")
+    console.log("DASHBOARD DID UPDATE")
   }
 
   fetchData(){
