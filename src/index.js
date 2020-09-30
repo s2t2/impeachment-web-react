44b27955e6178f316f8cfe1375c0f6c7fd610663
MJ Rossetti <s2t2@users.noreply.github.com>
2020-09-30 11:59:51 -0400

    Combining both layouts for now

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//import App from './App.js';
-import App from './_NavigableApp/App.js';
+import App from './App.js';
+//import App from './_NavigableApp/App.js';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
