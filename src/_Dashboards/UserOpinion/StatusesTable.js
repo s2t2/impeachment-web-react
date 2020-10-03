dd77871547967ac99f031dc58da3a20ad66bc92e
MJ Rossetti <s2t2@users.noreply.github.com>
2020-10-02 23:16:47 -0400

    Sort table by date ASC

--- src/_Dashboards/UserOpinion/StatusesTable.js
+++ src/_Dashboards/UserOpinion/StatusesTable.js
@@ -3,24 +3,14 @@
 import React from 'react';
 import { ReactComponent as UpArrow } from './arrow-upright.svg';
 
-import { format } from 'date-fns'
+import { format, compareAsc } from 'date-fns'
 
 export default function StatusesTable(props) {
-  //{
-  //   "created_at":"Wed, 22 Jan 2020 01:08:33 GMT",
-  //   "opinion_score":1,
-  //   "status_id":"1219788909678383105",
-  //   "status_text":"RT @AOC: \u201cNot me, Us\u201d means building a mass movement for social, economic, and racial justice.  And movement means we carry and care for on\u2026"
-  // },
 
-  //console.log("PROPS", this.props)
-  var tableRows = props.statuses.map(function(s){
-
+  var tableRows = props.statuses.sort(function(a, b){
+    return compareAsc(Date.parse(a["created_at"]), Date.parse(b["created_at"]))
+  }).map(function(s){
     var statusUrl = `https://twitter.com/politico/status/${s["status_id"]}`
-
-    //debugger;
-    //var date = Date.parse(s["created_at"])
-    //var date = new strftime('%Y-%m-%d', s["created_at"])
     var date = format(Date.parse(s["created_at"]), 'yyyy-MM-dd')
 
     return (
