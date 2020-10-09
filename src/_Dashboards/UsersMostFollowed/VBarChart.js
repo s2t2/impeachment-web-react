a68fcafd861ea929b12681ada017980ffde9f7d1
MJ Rossetti <s2t2@users.noreply.github.com>
2020-10-09 00:56:34 -0400

    WIP -trying some sliders

--- src/_Dashboards/UsersMostFollowed/VBarChart.js
+++ src/_Dashboards/UsersMostFollowed/VBarChart.js
@@ -4,6 +4,12 @@ import React, { PureComponent } from 'react'
 import { VictoryTheme, VictoryChart, VictoryBar, VictoryLabel } from 'victory';
 import {scaleSequential, interpolateRdBu, scaleLinear, scaleDiverging, scaleThreshold} from 'd3'
 
+
+
+
+import { Slider } from 'material-ui-slider';
+
+
 var colorScale = scaleSequential(interpolateRdBu).domain([1, 0]) // reverse so 0:blue and 1:red
 
 //var colorScale = scaleDiverging()
@@ -19,53 +25,77 @@ var colorScale = scaleSequential(interpolateRdBu).domain([1, 0]) // reverse so 0
 //    //.range(['#67001f','#b2182b','#d6604d','#f4a582','#fddbc7','#f7f7f7','#d1e5f0','#92c5de','#4393c3','#2166ac','#053061'])
 
 
+
+
+
 export default class MyBarChart extends PureComponent {
-    //constructor(props) {
-    //  super(props)
-    //  //this.state = {limit: 500, parsedResponse: null} // TODO: get screen name from input box or URL params (maybe use window.location.href and split it, or find some kind of react router property)
-    //  //this.fetchData = this.fetchData.bind(this)
-    //}
+    constructor(props) {
+      super(props)
+      this.state = {tweetMin: 3, parsedResponse: null} // TODO: get screen name from input box or URL params (maybe use window.location.href and split it, or find some kind of react router property)
+      //this.fetchData = this.fetchData.bind(this)
+      this.handleSlide = this.handleSlide.bind(this)
+    }
+
+    handleSlide(val){
+        this.setState({tweetMin: val})
+    }
+
     render() {
-        //var rows = this.props.users.map(function(user){
-        //  var scorePct = (user["avg_score_lr"] * 100.0).toFixed(1) + "%"
-        //  var userHandle = "@" + user["screen_name"]
-        //  var followerCount = user["follower_count"]
-
-        //  return <li> {userHandle} |  {followerCount} | ({scorePct}) </li>
-        //})
-
-
         var users = this.props.users.slice(0, 10).sort(function(a, b){
             return a["follower_count"] - b["follower_count"]
         }).map(function(user){
+            //  var scorePct = (user["avg_score_lr"] * 100.0).toFixed(1) + "%"
+            //  var userHandle = "@" + user["screen_name"]
+            //  var followerCount = user["follower_count"]
             user["handle"] = `@${user['screen_name']}`
             return user
         })
 
+
+
+
+
+
+        //var slider = <Slider value={this.state.tweetMin} orientation="horizontal" onChange={this.handleSlide}/>
+
+        var slider = <Slider min={3} max={10} defaultValue={3}></Slider>
+
+
+
+
+
+
+
+
+
         return (
-            <VictoryChart >
-                <VictoryBar
-                    horizontal
-                    data={users}
-                    x="handle"
-                    y="follower_count"
-                    animate={true}
+            <span>
+                {slider}
 
-                    //barWidth={12}
-                    barRatio={0.95}
-                    //alignment="middle"
+                <VictoryChart >
+                    <VictoryBar
+                        horizontal
+                        data={users}
+                        x="handle"
+                        y="follower_count"
+                        animate={true}
 
-                    labels={({ datum }) => datum["avg_score_lr"]}
-                    //labelComponent={<VictoryLabel dx={-30}/>}
-                    style={{
-                        data: {
-                            fill: ({ datum }) => colorScale(datum["avg_score_lr"]) // help I don't know how this syntax works. do we have to bind.this or something somewhere?
-                        }
-                    }}
+                        //barWidth={12}
+                        barRatio={0.95}
+                        //alignment="middle"
+
+                        labels={({ datum }) => datum["avg_score_lr"]}
+                        //labelComponent={<VictoryLabel dx={-30}/>}
+                        style={{
+                            data: {
+                                fill: ({ datum }) => colorScale(datum["avg_score_lr"])
+                            }
+                        }}
 
 
-                />
-            </VictoryChart>
+                    />
+                </VictoryChart>
+            </span>
         )
     }
 
