885edbf80462a49668260e0f42cc2477f2b5429d
MJ Rossetti <s2t2@users.noreply.github.com>
2020-10-09 17:06:43 -0400

    Clean this up a bit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App.js';
+//import App from './App.js';
 //import App from './_NavigableApp/App.js';
 //import App from './_Dashboards/UserOpinion/Dashboard.js';
 //import Dial from './_Dashboards/UserOpinion/Dial.js';
 //import App from './_Dashboards/About/Page.js';
-//import App from './_Dashboards/UsersMostFollowed/Dashboard.js';
+import App from './_Dashboards/UsersMostFollowed/Dashboard.js';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
