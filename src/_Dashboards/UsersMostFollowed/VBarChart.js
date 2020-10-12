d8f7888542db9240565dd409134b69691877c8d6
Mike Rossetti <s2t2@users.noreply.github.com>
2020-10-12 11:58:25 -0400

    MJR Sprint VII (#17)
    Added user category and opinion model selections to filter the user opinions dashboard.

--- src/_Dashboards/UsersMostFollowed/VBarChart.js
+++ src/_Dashboards/UsersMostFollowed/VBarChart.js
@@ -1,49 +1,40 @@
-import React, { PureComponent } from 'react'
+import React, { Component } from 'react'
 import Form from 'react-bootstrap/Form'
-import Container from 'react-bootstrap/Container'
+//import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-//import Spinner from 'react-bootstrap/Spinner'
-import { VictoryTheme, VictoryChart, VictoryBar, VictoryLabel } from 'victory';
-import {scaleSequential, interpolateRdBu, scaleLinear, scaleDiverging, scaleThreshold} from 'd3'
-//import { orderBy } from 'lodash';
+import {VictoryChart, VictoryBar, VictoryAxis} from 'victory' // VictoryTheme, VictoryLabel
+//import { orderBy } from 'lodash'
+import {scaleSequential, interpolateRdBu} from 'd3'
+import './VBarChart.css'
 
-import RangeSlider from 'react-bootstrap-range-slider';
-
-//import Slider, { Range } from 'rc-slider';
-//import Slider from 'rc-slider';
-//const createSliderWithTooltip = Slider.createSliderWithTooltip
-//const Range = createSliderWithTooltip(Slider.Range) // the tooltip doesn't move
-
-//import { Range } from 'rc-slider';
-
-import Slider from 'rc-slider';
-const { createSliderWithTooltip } = Slider;
+import RangeSlider from 'react-bootstrap-range-slider'
+//import { Range } from 'rc-slider'
+import Slider from 'rc-slider'
+const { createSliderWithTooltip } = Slider
 const Range = createSliderWithTooltip(Slider.Range);
 
+const colorScale = scaleSequential(interpolateRdBu).domain([1, 0]) // reverse so 0:blue and 1:red
 
-var colorScale = scaleSequential(interpolateRdBu).domain([1, 0]) // reverse so 0:blue and 1:red
+function formatBigNumber(num) {
+    return Math.abs(num) > 999 ? Math.sign(num)*((Math.abs(num)/1000).toFixed(1)) + 'K' : Math.sign(num)*Math.abs(num)
+} // h/t: https://stackoverflow.com/a/9461657
 
-//var colorScale = scaleDiverging()
-//    .domain([1, 0.5, 0]) // reverse so 0:blue and 1:red
-//    .range(['#b2182b','#f7f7f7','#2166ac'])
-
-//var colorScale = scaleDiverging(interpolateRdBu)
-//    .domain([1, 0.5, 0]) // reverse so 0:blue and 1:red
-
-//var colorScale = scaleLinear()
-//    .domain([1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0]) // reverse so 0:blue and 1:red
-//    .range(['#b2182b','#d6604d','#f4a582','#fddbc7','#f7f7f7','#d1e5f0','#92c5de','#4393c3','#2166ac'])
-//    //.range(['#67001f','#b2182b','#d6604d','#f4a582','#fddbc7','#f7f7f7','#d1e5f0','#92c5de','#4393c3','#2166ac','#053061'])
-
-export default class MyBarChart extends PureComponent {
+export default class MyBarChart extends Component {
     constructor(props) {
       super(props)
-      this.state = {tweetMin: 3, opinionRange: [0, 100]} // TODO: get URL params from router, so we can make custom charts and link people to them, like ?opinionMin=40&opinionMax=60&tweetMin=10
+      this.state = {
+        tweetMin: 5,
+        opinionRange: [0, 100],
+        userCategories: ["MAJOR-MEDIA-OUTLET","ELECTED-OFFICIAL","PARTY", "OTHER"],
+        opinionModel: "lr"
+    } // TODO: get URL params from router, so we can make custom charts and link people to them, like ?opinionMin=40&opinionMax=60&tweetMin=10
       this.handleTweetMinChange = this.handleTweetMinChange.bind(this)
       this.handleOpinionRangeChange = this.handleOpinionRangeChange.bind(this)
-      this.handleOpinionMinChange = this.handleOpinionMinChange.bind(this)
-      this.handleOpinionMaxChange = this.handleOpinionMaxChange.bind(this)
+      //this.handleOpinionMinChange = this.handleOpinionMinChange.bind(this)
+      //this.handleOpinionMaxChange = this.handleOpinionMaxChange.bind(this)
+      this.handleCategoryCheck = this.handleCategoryCheck.bind(this)
+      this.handleModelSelect = this.handleModelSelect.bind(this)
     }
 
     handleTweetMinChange(changeEvent){
@@ -55,25 +46,50 @@ export default class MyBarChart extends PureComponent {
         this.setState({opinionRange: newRange})
     }
 
-    handleOpinionMinChange(changeEvent){
-        console.log("CHANGE OPINION MIN", changeEvent.target.value)
-        //var opinionMin = parseFloat(changeEvent.target.value) // convert string to decimal, but they end up as NaN. TODO: (maybe take user inputs as integers)
-        var opinionMin = changeEvent.target.value
-        var opinionMax = this.state.opinionRange[1]
-        this.setState({opinionRange: [changeEvent.target.value, opinionMax]})
+    //handleOpinionMinChange(changeEvent){
+    //    console.log("CHANGE OPINION MIN", changeEvent.target.value)
+    //    //var opinionMin = parseFloat(changeEvent.target.value) // convert string to decimal, but they end up as NaN. TODO: (maybe take user inputs as integers)
+    //    var opinionMin = changeEvent.target.value
+    //    var opinionMax = this.state.opinionRange[1]
+    //    this.setState({opinionRange: [changeEvent.target.value, opinionMax]})
+    //}
+
+    //handleOpinionMaxChange(changeEvent){
+    //    console.log("CHANGE OPINION MAX", "FROM", this.state.opinionRange, "TO", changeEvent.target.value)
+    //    var opinionMin = this.state.opinionRange[0]
+    //    //var opinionMax = parseFloat(changeEvent.target.value) // convert string to decimal, but they end up as NaN. TODO: (maybe take user inputs as integers)
+    //    var opinionMax = changeEvent.target.value
+    //    this.setState({opinionRange: [opinionMin, opinionMax]})
+    //}
+
+    handleCategoryCheck(changeEvent){
+        var category = changeEvent.target.value
+        console.log("CHECK CATEGORY:", category)
+
+        var userCategories = this.state.userCategories
+        var categoryIndex = userCategories.indexOf(category) // will be -1 if item not in array
+        if (categoryIndex >= 0 ) {
+            userCategories.splice(categoryIndex, 1) // remove 1 item from array at the given position
+        } else {
+            userCategories.push(category)
+        }
+
+        console.log("CATEGORIES:", userCategories)
+        this.setState({userCategories: userCategories}) // this is updating state but why not triggering a re-render?
     }
 
-    handleOpinionMaxChange(changeEvent){
-        console.log("CHANGE OPINION MAX", "FROM", this.state.opinionRange, "TO", changeEvent.target.value)
-        var opinionMin = this.state.opinionRange[0]
-        //var opinionMax = parseFloat(changeEvent.target.value) // convert string to decimal, but they end up as NaN. TODO: (maybe take user inputs as integers)
-        var opinionMax = changeEvent.target.value
-        this.setState({opinionRange: [opinionMin, opinionMax]})
+    handleModelSelect(changeEvent){
+        var model = changeEvent.target.value
+        console.log("SELECT MODEL:", model)
+        this.setState({"opinionModel": model})
     }
 
     render() {
         var tweetMin = this.state.tweetMin
         var opinionRange = this.state.opinionRange
+        var userCategories = this.state.userCategories
+        var opinionModel = this.state.opinionModel
+        var opinionMetric = `avg_score_${opinionModel}`
         var barCount = this.props.barCount || 10 // would be nice to get 15 or 20 to work (with smaller bar labels)
 
         // FILTER AND SORT USERS
@@ -81,12 +97,13 @@ export default class MyBarChart extends PureComponent {
             .filter(function(user){
                 return (
                     user["status_count"] >= tweetMin &&
-                    user["avg_score_lr"] * 100.0 >= opinionRange[0] &&
-                    user["avg_score_lr"] * 100.0 <= opinionRange[1]
+                    user[opinionMetric] * 100.0 >= opinionRange[0] &&
+                    user[opinionMetric] * 100.0 <= opinionRange[1] &&
+                    userCategories.includes(user["category"])
             )})
             .map(function(user){
                 user["handle"] = `@${user['screen_name']}`
-                user["scorePct"] = (user["avg_score_lr"] * 100.0).toFixed(1) + "%"
+                user["scorePct"] = (user[opinionMetric] * 100.0).toFixed(1) + "%"
                 return user
             })
             .sort(function(a, b){
@@ -98,48 +115,127 @@ export default class MyBarChart extends PureComponent {
             <span>
                 <Form >
                     <Form.Group as={Row}>
-                        <Col xs="3">
+                        <Col xs="5">
                             <Form.Label>Minimum Tweet Count</Form.Label>
 
                             <RangeSlider min={3} max={200}
                                 value={tweetMin}
                                 onChange={this.handleTweetMinChange}
-                                tooltip="auto" // "on" //"auto"
+                                tooltip="on" // "on" //"auto"
                                 tooltipPlacement="bottom"
                                 //tooltipLabel=
                                 //variant="dark"
                             />
-                        </Col>
+                        </Col> {/*}
                         <Col xs="2" style={{"paddingTop":"1.9em"}}>
                             <Form.Control type="number" min={3} max={200} value={tweetMin} onChange={this.handleTweetMinChange}/>
-                        </Col>
+                        </Col> */}
 
                         <Col xs="1">
                         </Col>
 
-                        <Col xs="4">
+                        <Col xs="5">
                             <Form.Label>Mean Opinion Score</Form.Label>
 
                             <Range min={0} max={100} step={1}
                                 defaultValue={[0, 100]}
                                 value={opinionRange}
-                                marks={{0: "Pro-Impeachment (D)", 100: "Anti-Impeachment (R)"}}
+                                marks={{
+                                    0: "Pro-Impeachment",
+                                    10: "",
+                                    20: "",
+                                    30: "",
+                                    40: "",
+                                    50: "Neutral",
+                                    60: "",
+                                    70: "",
+                                    80: "",
+                                    90: "",
+                                    100: "Pro-Trump"}}
                                 onChange={this.handleOpinionRangeChange}
                                 allowCross={false}
+                                //pushable={10}
                                 tooltip="auto"
                                 tipFormatter={value => `${value}%` }
-                                tipProps={{placement: "top", visible: true}}
+                                tipProps={{
+                                    visible: true,
+                                    placement: "top" // "top" "bottom"
+                                }}
                             />
                         </Col>
 
+                        {/*}
                         <Col xs="2" style={{"paddingTop":"1em"}}>
                             <Form.Control type="number" min={0} max={99} value={opinionRange[0]} onChange={this.handleOpinionMinChange}/>
                             <Form.Control type="number" min={1} max={100} value={opinionRange[1]} onChange={this.handleOpinionMaxChange}/>
+                        </Col>  */}
+                    </Form.Group>
+
+
+                    <Form.Group as={Row} id="user-category-checks">
+                        <Col xs="5">
+                            <Form.Label>User Category</Form.Label>
+
+                            <div key="inline-checkbox" className="mb-3">
+                                <Form.Check inline label="Media Outlet" value="MAJOR-MEDIA-OUTLET" type="checkbox" id="check-major-media-outlet"
+                                    checked={userCategories.includes("MAJOR-MEDIA-OUTLET")}
+                                    onChange={this.handleCategoryCheck}
+                                />
+
+                                <Form.Check inline label="Elected Official" value="ELECTED-OFFICIAL" type="checkbox" id="check-elected-official"
+                                    checked={userCategories.includes("ELECTED-OFFICIAL")}
+                                    onChange={this.handleCategoryCheck}
+                                />
+                                <Form.Check inline label="Political Party" value="PARTY" type="checkbox" id="check-party"
+                                    checked={userCategories.includes("PARTY")}
+                                    onChange={this.handleCategoryCheck}
+                                />
+                                <Form.Check inline label="Others" value="OTHER" type="checkbox" id="check-others"
+                                    checked={userCategories.includes("OTHER")}
+                                    onChange={this.handleCategoryCheck}
+                                />
+                            </div>
+                        </Col>
+
+                        <Col xs="1">
+                        </Col>
+
+                        <Col xs="5">
+                            <Form.Label>Opinion Model</Form.Label>
+
+                            <div key="inline-radios" className="mb-3">
+                                <Form.Check inline label="Logistic Regression" value="lr" type="radio" id="radio-lr"
+                                    checked={opinionModel === "lr"}
+                                    onChange={this.handleModelSelect}
+                                />
+                                <Form.Check inline label="Naive Bayes" value="nb" type="radio" id="radio-nb"
+                                    checked={opinionModel === "nb"}
+                                    onChange={this.handleModelSelect}
+                                />
+                            </div>
+
                         </Col>
                     </Form.Group>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                 </Form>
 
-                <VictoryChart padding={{ left: 175, top: 15, right: 50, bottom: 130 }} >
+                <VictoryChart
+                    padding={{ left: 175, top: 15, right: 50, bottom: 130 }} // spacing for axis labels (screen names)
+                    domainPadding={{ x: [10,0] }} // spacing between bottom bar and bottom axis
+                >
                     <VictoryBar horizontal data={users} x="handle" y="follower_count"
                         animate={true}
                         //barWidth={12}
@@ -150,9 +246,48 @@ export default class MyBarChart extends PureComponent {
                         //labelComponent={<VictoryLabel dx={-30}/>}
                         style={{
                             data: {
-                                fill: ({ datum }) => colorScale(datum["avg_score_lr"])
+                                fill: ({ datum }) => colorScale(datum[opinionMetric]),
+                                //stroke: ({ index }) => +index % 2 === 0  ? "#000000" : "#c43a31",
+                                //fillOpacity: 0.7,
+                                //strokeWidth: 3
+                            },
+                            labels: {
+                                fontSize: 10,
+                                padding:2
+                                //fill: ({ datum }) => datum.x === 3 ? "#000000" : "#c43a31"
                             }
                         }}
+
+
+
+
+
+
+
+
+
+
+
+                    />
+                    <VictoryAxis
+                        //label="Screen Name"
+                        //tickFormat={["a", "b", "c", "d", "e"]}
+                    />
+                    <VictoryAxis dependentAxis
+                        //tickFormat={(tick) => `${tick}%`}
+                        tickFormat={formatBigNumber}
+                        label="Follower Count"
+
+                        style={{
+                            //axis: {stroke: "#756f6a"},
+                            axisLabel: {fontSize: 10, padding:25},
+                            //grid: {stroke: ({ tick }) => tick > 0.5 ? "red" : "grey"},
+                            ticks: {stroke: "grey", size: 4},
+                            tickLabels: {fontSize: 10, padding:5}
+                        }}
+
+
+
                     />
                 </VictoryChart>
             </span>
