da5e75bf7a3ee38b6992beb0702632a1d4ac7531
MJ Rossetti <s2t2@users.noreply.github.com>
2020-10-15 00:32:13 -0400

    Be agile and post more cool static images

--- src/_App/BotAnalysis/CommunityActivity/Section.js
+++ src/_App/BotAnalysis/CommunityActivity/Section.js
@@ -6,8 +6,9 @@ import Container from 'react-bootstrap/Container'
 import Card from 'react-bootstrap/Card'
 //import Table from 'react-bootstrap/Table'
 
-import UsersMostRetweeted from "./UsersMostRetweeted" // "../../_Dashboards/UsersMostRetweeted/Dashboard"
-import StatusesMostRetweeted from "./StatusesMostRetweeted" // "../../_Dashboards/StatusesMostRetweeted/Dashboard"
+import UsersMostRetweeted from "./UsersMostRetweeted"
+import StatusesMostRetweeted from "./StatusesMostRetweeted"
+import communityRetweetsOverTime from "./daily-rts-by-community.png"
 
 export default function BotCommunityAnalysis() {
 
@@ -18,12 +19,20 @@ export default function BotCommunityAnalysis() {
                     <Card.Title><h3>Bot Community Activity</h3></Card.Title>
 
                     <Card.Text>
-                        By examining the retweet beneficiaries of each bot community, we observe Community 0 represents left-leaning bots (pro-Impeachment), while Community 1 represents right-leaning bots (pro-Trump). We also observe that left-leaning bots retweet a greater variety of users, while right-leaning bots retweet Trump most by-far.
+                        By examining the retweet beneficiaries of each bot community, we observe Community 0 represents left-leaning bots (pro-Impeachment), while Community 1 represents right-leaning bots (pro-Trump).
+                        {" "} We also observe that left-leaning bots retweet a greater variety of users, while right-leaning bots retweet Trump significantly more than any other user.
                     </Card.Text>
 
                     <UsersMostRetweeted/>
 
                     <StatusesMostRetweeted/>
+
+                    <Card.Text>
+                        Left-leaning bots outnumber right-leaning bots 571 to 110, and generate a greater volume of tweets than right-leaning bots.
+                    </Card.Text>
+
+                    <img src={communityRetweetsOverTime} alt="a histogram depicting bot probabilities" style={{marginTop:20, marginBottom:20}} class="img-fluid"/>
+
                 </Card.Body>
             </Card>
         </Container>
