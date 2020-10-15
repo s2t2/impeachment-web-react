003ee03cbed721dca17a97ac9a7c7296fa33bfd0
MJ Rossetti <s2t2@users.noreply.github.com>
2020-10-15 19:29:47 -0400

    Edit About

--- src/_App/About.js
+++ src/_App/About.js
@@ -35,24 +35,24 @@ export default function About() {
                     <ul>
                         <li className="font-weight-bold">I. Tweet Collection</li>
                         <ul>
-                            <li><a href="/collection-topics">A. Collection Topics</a></li>
-                            <li><a href="/collection-timeline">B. Collection Timeline</a></li>
-                            <li><a href="/collection-results">C. Collection Results</a></li>
+                            <li><a href="/collection-topics">Collection Topics</a></li>
+                            <li><a href="/collection-timeline">Collection Timeline</a></li>
+                            <li><a href="/collection-results">Collection Results</a></li>
                         </ul>
 
                         <li className="font-weight-bold">II. Bot Analysis</li>
                         <ul>
-                            <li><a href="/bot-classification">A. Bot Classification</a></li>
-                            <li><a href="/bot-communities">B. Bot Communities</a></li>
-                            <li><a href="/bot-language">C. Bot Language</a></li>
-                            <li><a href="/bot-impact">D. Bot Impact</a></li>
+                            <li><a href="/bot-classification">Bot Classification</a></li>
+                            <li><a href="/bot-communities">Bot Communities</a></li>
+                            <li><a href="/bot-language">Bot Language</a></li>
+                            <li><a href="/bot-impact">Bot Impact</a></li>
                         </ul>
 
                         <li className="font-weight-bold">III. Opinion Analysis</li>
                         <ul>
-                            <li><a href="/opinion-models">A. Opinion Models</a></li>
-                            <li><a href="/user-opinions">B. User Opinions</a></li>
-                            <li><a href="/top-user-opinions">C. Top User Opinions</a></li>
+                            <li><a href="/opinion-models">Opinion Models</a></li>
+                            <li><a href="/user-opinions">User Opinions</a></li>
+                            <li><a href="/top-user-opinions">Top User Opinions</a></li>
                         </ul>
                     </ul>
                 </Card.Body>
