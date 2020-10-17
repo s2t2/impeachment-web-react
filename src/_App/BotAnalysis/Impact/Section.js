10edf4bfe41d99d8748e9f3ac017b32257f63fa9
Mike Rossetti <s2t2@users.noreply.github.com>
2020-10-17 16:45:31 -0400

    Bot Impact (#27)
    Post static images about bot impact, along with data so we can make dynamic charts in a subsequent iteration.

--- src/_App/BotAnalysis/Impact/Section.js
+++ src/_App/BotAnalysis/Impact/Section.js
@@ -6,6 +6,9 @@ import Container from 'react-bootstrap/Container'
 import Card from 'react-bootstrap/Card'
 //import Table from 'react-bootstrap/Table'
 
+import dailyEquilibrium from "./daily-opinion-equilibrium-tableau.png"
+import dailyShift from "./daily-opinion-shift-tableau.png"
+
 export default function BotImpact() {
     return (
         <Container fluid>
@@ -14,8 +17,19 @@ export default function BotImpact() {
                     <Card.Title><h3>Bot Impact</h3></Card.Title>
 
                     <Card.Text>
-                        Coming Soon...
+                        For each day in our primary collection period, we calculated the average opinion scores for all users, with vs. without bots, to assess the impact the bots were having on the conversation.
+                        {" "} We observe the mean daily opinion score without bots is 46.6%, while with bots is 45.2%.
+                        {" "} This suggests there is an average daily opinion shift of 1.4% towards left-leaning bot opinions.
                     </Card.Text>
+                    <img src={dailyEquilibrium} alt="a line graph of daily opinion with bots vs without" style={{marginTop:20, marginBottom:20}} class="img-fluid"/>
+
+                    <Card.Text>
+                        The chart below shows the shift in opinion scores by day.
+                        {" "} The highest daily shift towards right-leaning bot opinions was 2.8% on 12/28, and the highest daily shift towards left-leaning bot opinions was 6.3% on 1/8.
+                    </Card.Text>
+                    <img src={dailyShift} alt="a bar graph of daily opinion shift by bot community" style={{marginTop:20, marginBottom:20}} class="img-fluid"/>
+
+
                 </Card.Body>
             </Card>
         </Container>
