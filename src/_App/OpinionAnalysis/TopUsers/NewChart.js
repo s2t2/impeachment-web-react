50bd47f3ca5b3972ebf57a64259f6013aa5a76fa
MJ Rossetti <s2t2@users.noreply.github.com>
2020-10-16 21:02:32 -0400

    Rendering a chart

--- src/_App/OpinionAnalysis/TopUsers/NewChart.js
+++ src/_App/OpinionAnalysis/TopUsers/NewChart.js
@@ -1,74 +1,133 @@
 
 import React from 'react'
-//import {VictoryChart, VictoryBar, VictoryAxis, VictoryLegend} from 'victory' // VictoryLabel
-//import {scaleSequential, interpolateRdBu} from 'd3'
-//import { orderBy } from 'lodash'
+import {VictoryChart, VictoryBar, VictoryAxis, VictoryLegend, VictoryLabel} from 'victory'
+import {scaleSequential, interpolateRdBu} from 'd3'
 
 import './NewDashboard.css'
 
-//const colorScale = scaleSequential(interpolateRdBu).domain([1, 0]) // reverse so 0:blue and 1:red
+const colorScale = scaleSequential(interpolateRdBu).domain([1, 0]) // reverse so 0:blue and 1:red
 
-//function formatBigNumber(num) {
-//    return Math.abs(num) > 999 ? Math.sign(num)*((Math.abs(num)/1000).toFixed(1)) + 'K' : Math.sign(num)*Math.abs(num)
-//} // adds "K" for thousands h/t: https://stackoverflow.com/a/9461657
+function formatBigNumber(num) {
+    return Math.abs(num) > 999 ? Math.sign(num)*((Math.abs(num)/1000).toFixed(1)) + 'K' : Math.sign(num)*Math.abs(num)
+} // adds "K" for thousands h/t: https://stackoverflow.com/a/9461657
 
-const OPINION_MODEL_NAMES = {"avg_score_lr": "Logistic Regression", "avg_score_nb": "Naive Bayes", "avg_score_bert": "BERT Transformer"}
 const ALL_CATEGORIES = ["ELECTED-OFFICIAL", "PARTY", "GOVERNMENT", "MAJOR-MEDIA-OUTLET", "MEDIA-OUTLET"]
+const BAR_COUNT = 10 // would be nice to get 15 or 20 to work (with smaller bar labels)
+const MODEL_LABELS = {"avg_score_lr": "Logistic Regression", "avg_score_nb": "Naive Bayes", "avg_score_bert": "BERT Transformer"}
+const METRIC_LABELS = {
+    "follower_count": "Follower Count",
+    "status_count": "Tweet Count",
+    "avg_score_lr": "Opinion Score (Logistic Regression)",
+    "avg_score_nb": "Opinion Score (Naive Bayes)",
+    "avg_score_bert": "Opinion Score (BERT Transformer)"
+}
 
 export default function NewChart(props) {
 
-    //
     // CHART OPTIONS
-    //
 
+    var metric = props["metric"] || "follower_count"
+    var sortOrder = props["sortOrder"] || "DESC" // "ASC" // "DESC"
+    var opinionMetric = props["opinionMetric"] || "avg_score_lr" // "avg_score_lr" // "avg_score_nb" // "avg_score_bert"
+    var opinionMin = parseInt(props["opinionMin"]) || 0 // must be between 0 and 99 (and > opinionMax)
+    var opinionMax = parseInt(props["opinionMax"]) || 100 // must be between 1 and 100 (and < opinionMin)
+    var tweetMin = parseInt(props["tweetMin"]) || 5 // must be between 3 and 200
     var categories = props["categories"] || ALL_CATEGORIES
-    var tweetMin = parseInt(props["tweetMin"]) || 5
-    var opinionMin = parseInt(props["opinionMin"]) || 0
-    var opinionMax = parseInt(props["opinionMax"]) || 100
-    //var opinionRange = [opinionMin, opinionMax]
 
-    var opinionMetric = props["metric"] || "avg_score_lr"
-    var opinionModel = OPINION_MODEL_NAMES[opinionMetric]
-    var sortMetric = "Mean Opinion Score"
-    var sortOrder = props["sort"] || "DESC"
-    var barCount = 10 // would be nice to get 15 or 20 to work (with smaller bar labels)
-
-
-    //
     // CHART DATA
+
+    var slice = {"ASC": BAR_COUNT, "DESC": -BAR_COUNT}[sortOrder] // negative number takes last X users (which is actually the top X users)
+
+    var users = props["users"]
+        .filter(function(user){
+            return (user["status_count"] >= tweetMin
+                && user[opinionMetric] * 100.0 >= opinionMin
+                && user[opinionMetric] * 100.0 <= opinionMax
+                && categories.includes(user["category"])
+        )})
+        .map(function(user){
+            user["handle"] = `@${user['screen_name']}`
+            user["scorePct"] = (user[opinionMetric] * 100.0).toFixed(1) + "%"
+            return user
+        })
+        .sort(function(a, b){
+            return a[metric] - b[metric] // chart wants this order
+        }) // sort before slice
+        .slice(slice)
+
     //
+    // CHART FORMATTING
+    //
+
+    var opinionModel = MODEL_LABELS[opinionMetric]
+    var metricLabel = METRIC_LABELS[metric]
 
     var chartTitle = "Users Tweeting about Trump Impeachment"
     var chartFooter = `Chart shows users tweeting at least ${tweetMin} times about Trump Impeachment`
         chartFooter += ` whose mean opinion score is between ${opinionMin}% and ${opinionMax}%`
         chartFooter += ` (as scored by our ${opinionModel} opinion model).`
         //chartFooter += ` .`
-        //chartFooter += ` sorted by ${sortMetric}`
+        //chartFooter += ` sorted by ${metric}`
         //chartFooter += ` Includes only users categorized as: ${categories}`
 
-    //var users = props["users"]
-    //    .filter(function(user){
-    //        return (user["status_count"] >= tweetMin
-    //            && user[opinionMetric] * 100.0 >= opinionMin
-    //            && user[opinionMetric] * 100.0 <= opinionMax
-    //            && categories.includes(user["category"])
-    //    )})
-    //    .map(function(user){
-    //        user["handle"] = `@${user['screen_name']}`
-    //        user["scorePct"] = (user[opinionMetric] * 100.0).toFixed(1) + "%"
-    //        return user
-    //    })
-    //    .sort(function(a, b){
-    //        return a["follower_count"] - b["follower_count"] // chart wants this order
-    //    }) // sort before slice
-    //    .slice(-barCount) // negative number takes last X users (which is actually the top X users)
+    var chartPadding = {left: 175, top: 15, right: 50, bottom: 130} // spacing for axis labels (screen names)
+    var domainPadding = {x: [10,0]} // spacing between bottom bar and bottom axis
+    var chartTitleStyle = {marginTop: 10, marginBottom: 0}
+    var legendData = [
+        {name: "Pro-Impeachment (0%)", symbol: {fill: colorScale(0.15), type: "circle"}},
+        //{name: "Neutral (50%)",        symbol: {fill: colorScale(0.50), type: "circle"}},
+        {name: "Pro-Trump (100%)",     symbol: {fill: colorScale(0.85), type: "circle"}},
+    ]
+    var legendStyle = {
+        //border: {stroke: "black"},
+        title: {fontSize: 10},
+        data: {fontSize: 10},
+        labels: {fontSize: 10},
+    }
+    var barStyle = {
+        data: {
+            fill: ({ datum }) => colorScale(datum[opinionMetric]),
+            //stroke: ({ index }) => +index % 2 === 0  ? "#000000" : "#c43a31",
+            //fillOpacity: 0.7,
+            //strokeWidth: 3
+        },
+        labels: {
+            fontSize: 10,
+            padding:2
+            //fill: ({ datum }) => datum.x === 3 ? "#000000" : "#c43a31"
+        }
+    }
+    var axisStyle = {
+        //axis: {stroke: "#756f6a"},
+        axisLabel: {fontSize: 10, padding:25},
+        //grid: {stroke: ({ tick }) => tick > 0.5 ? "red" : "grey"},
+        ticks: {stroke: "grey", size: 4},
+        tickLabels: {fontSize: 10, padding:5}
+    }
 
     return (
         <span>
-            <p className="app-center chart-title-p" style={{marginTop:10, marginBottom:0}}>{chartTitle}</p>
-            <h4 className="app-center chart-title-h4" style={{marginTop:10, marginBottom:0}}>{chartTitle}</h4>
+            <p className="app-center chart-title-p" style={chartTitleStyle}>{chartTitle}</p>
+            <h4 className="app-center chart-title-h4" style={{chartTitleStyle}}>{chartTitle}</h4>
 
-            <p style={{marginLeft:30, marginRight:30}}>{chartFooter}</p>
+            <VictoryLegend orientation="horizontal" style={legendStyle} x={120} y={0} height={15}
+                data={legendData}
+            />
+
+            <VictoryChart padding={chartPadding} domainPadding={domainPadding}>
+                <VictoryBar horizontal style={barStyle} animate={true} barRatio={0.87}
+                    data={users} x="handle" y={metric}
+                    labels={({ datum }) => datum["scorePct"]}
+                />
+                <VictoryAxis/>
+                <VictoryAxis dependentAxis style={axisStyle} tickFormat={formatBigNumber}
+                    label={metricLabel}
+                />
+
+                <VictoryLabel text={chartFooter} y={215} style={{fill: "#000", fontSize:6}}/>
+
+            </VictoryChart>
+
         </span>
     )
 }
--- src/_App/OpinionAnalysis/TopUsers/NewDashboard.js
+++ src/_App/OpinionAnalysis/TopUsers/NewDashboard.js
@@ -5,10 +5,14 @@ import Container from 'react-bootstrap/Container'
 import Card from 'react-bootstrap/Card'
 
 import NewChart from './NewChart'
-
+import Spinner from '../../Spinner.js'
+import cachedData from './data.js'
 import './NewDashboard.css'
 
-export default class MyBarChart extends PureComponent {
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000"
+const CACHE_MODE = process.env.REACT_APP_CACHE_MODE || true // TODO: convert env var to bool
+
+export default class Dashboard extends PureComponent {
     constructor(props) {
         super(props)
 
@@ -20,25 +24,60 @@ export default class MyBarChart extends PureComponent {
         var tweetMin = parseInt(params["tweetMin"]) || 5
         var categories = parseInt(params["categories"]) || ["ELECTED-OFFICIAL", "PARTY", "GOVERNMENT", "MAJOR-MEDIA-OUTLET", "MEDIA-OUTLET"]
 
-        this.state = {metric: metric, sort: sort, opinionMin: opinionMin, opinionMax: opinionMax, tweetMin: tweetMin, categories: categories}
+        this.state = {
+            limit: 1000,
+            parsedResponse: null,
+            chartOptions: {metric: metric, sort: sort, opinionMin: opinionMin, opinionMax: opinionMax, tweetMin: tweetMin, categories: categories}
+        }
     }
 
     render() {
+        var chartData = this.state.parsedResponse
+        var chartOptions = this.state.chartOptions
+
+        var spinIntoStuff = <Spinner/>
+        if (chartData) {
+            console.log("USER CHART DATA:", chartData)
+            spinIntoStuff = <NewChart users={chartData} options={chartOptions}/>
+        }
+
         return (
             <Container fluid>
                 <Card>
-                    <NewChart/>
+                    {spinIntoStuff}
                 </Card>
             </Container>
         )
     }
 
+    fetchData(){
+        var requestUrl = `${API_URL}/api/v1/users_most_followed?limit=${this.state.limit}`
+        console.log("REQUEST URL:", requestUrl)
+        fetch(requestUrl)
+            .then(function(response) {
+                //console.log("RAW RESPONSE", "STATUS", response.status, response.statusText, response.ok, "HEADERS", response.headers, response.url)
+                return response.json()
+            })
+            .then(function(json){
+                console.log("FETCHED", json.length, "ITEMS")
+                this.setState({parsedResponse: json})
+            }.bind(this))
+            .catch(function(err){
+                console.error("FETCH ERR", err)
+            })
+    }
+
     componentDidMount(){
-        console.log("BAR CHART DID MOUNT")
+        console.log("DASHBOARD DID MOUNT")
+        if (CACHE_MODE) {
+            this.setState({parsedResponse: cachedData})
+        } else {
+            this.fetchData()
+        }
     }
 
     componentDidUpdate(prevProps) {
-        console.log("BAR CHART DID UPDATE")
+        console.log("DASHBOARD DID UPDATE")
     }
 
 }
