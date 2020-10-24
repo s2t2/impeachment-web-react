b8d7cc20922a1ce68a87f785810afdf00d856f89
MJ Rossetti <s2t2@users.noreply.github.com>
2020-10-24 11:15:29 -0400

    Cache some data so we can make the charts faster, but the wordclouds need refactoring

--- src/_App/Utils/Colors.js
+++ src/_App/Utils/Colors.js
@@ -3,14 +3,10 @@ import {scaleSequential, interpolateRdBu} from 'd3'
 
 const colorScale = scaleSequential(interpolateRdBu).domain([1, 0]) // reverse so 0:blue and 1:red
 
-const legendBlue = "steelblue" // colorScale(0.15)
-const legendRed = "#D62021" // colorScale(0.85)
+const legendBlue = "steelblue" // colorScale(0.15) // "flag" blue: "#002868"
+const legendRed = "#D62021" // colorScale(0.85) // "flag" red: "#bf0a30"
 const legendNeutral = "#ccc"
 
 export {
     colorScale, legendBlue, legendRed, legendNeutral
 }
-
-// old "flag" colors:
-// blue: "#002868"
-// red: "#bf0a30"
