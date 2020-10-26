389e2395603e7c8b0de2cb5430546ff8cd13b18a
MJ Rossetti <s2t2@users.noreply.github.com>
2020-10-26 12:48:34 -0400

    Call it out for what it is

--- src/_App/BotAnalysis/Language/Section.js
+++ src/_App/BotAnalysis/Language/Section.js
@@ -18,26 +18,23 @@ export default function BotLanguageSection() {
             <Card>
                 <Card.Body>
                     <Card.Title><h3>Bot Language</h3></Card.Title>
+
                     <Card.Text>
-                        We analyzed the language patterns exhibited by each <a href="/bot-communities">bot community</a>, to determine which terms and hashtags appeared most frequently in tweets by members of that community.
+                        We analyzed the language patterns exhibited by each <a href="/bot-communities">bot community</a>,
+                        {" "}to determine which terms and hashtags appeared most frequently in tweets by members of that community.
+                        We also analyzed which hashtags appeared most frequently in bot profiles.
+                    </Card.Text>
+                    <Card.Text>
+                        It is interesting to observe Pro-Trump bots using hashtags like <i>'#QANON'</i>, <i>'#WWG1WGA'</i>, and <i>'#GREATAWAKENING'</i>,
+                        {" "} which are related to the <a href="https://www.cbsnews.com/news/what-is-the-qanon-conspiracy-theory/">"Q-Anon" conspiracy theory</a>.
                     </Card.Text>
 
                     <StatusHashtags/>
                     <StatusHashtagWordClouds/>
 
-                    <Card.Text>
-                        We also analyzed which terms and hashtags appeared most frequently in bot profiles.
-                    </Card.Text>
-
-
                     <ProfileHashtags/>
                     <ProfileHashtagWordClouds/>
 
-                    {/*
-                    <Card.Text>
-                        NOTE: the hashtags <i>'#QAnon'</i>, <i>'#WWG1WGA'</i>, and <i>'#GreatAwakening'</i> are related to the <a href="https://www.cbsnews.com/news/what-is-the-qanon-conspiracy-theory/">"Q-Anon" conspiracy theory</a>.
-                    </Card.Text>
-                    */}
                 </Card.Body>
             </Card>
         </Container>
