9b277ac6f125dbe9ea4da939735bab4ffdb2ccb1
MJ Rossetti <s2t2@users.noreply.github.com>
2020-10-27 16:23:27 -0400

    Graph container

--- src/_App/BotAnalysis/Networks/ForceGraph.js
+++ src/_App/BotAnalysis/Networks/ForceGraph.js
@@ -17,53 +17,28 @@ export default class DailyOpinionShift extends PureComponent {
     }
 
     render() {
-        var spinIntoChart = <Spinner/>
-
-        if(this.state.parsedResponse){
-            var data = this.state.parsedResponse
-            console.log("NETWORK GRAPH DATA", data)
-
-            //data = data.map(function(daily){
-            //    daily["mean_opinion_shift"] = daily["mean_opinion_equilibrium_bot"] - daily["mean_opinion_equilibrium_nobot"]
-            //    return daily
-            //})
-            //console.log("DAILY OPINION SHIFT DATA 2", data)
-
-            const chartTitle = `Bot Networks`
-            //const chartSubtitle = `Opinion Model: BERT Transformer`
-
-            spinIntoChart = (
-                <span>
-                    <Card.Text className="app-center">
-                        {chartTitle}
-                        {/*
-                        <br/>
-                        <small>{chartSubtitle}</small>
-                        */}
-                    </Card.Text>
-
-                    <div style={{width: "100%", height: 500}}>
-                        <p>There will be a graph here.</p>
-                    </div>
-                </span>
-            )
-        }
+        //var spinIntoChart = <Spinner/>
 
         return (
             <Card style={{marginBottom:0}}>
                 <Card.Body>
-                    {spinIntoChart}
+                   <Card.Text className="app-center">
+                        Bot Networks
+                    </Card.Text>
+
+                    <div className="force-graph-container"
+                        // style={{width: "100%", height: 500}}
+                    >
+                    </div>
                 </Card.Body>
             </Card>
         )
     }
 
     componentDidMount() {
-        console.log("DASHBOARD DID MOUNT")
-        //this.fetchData()
-        //setTimeout(function(){
-        //    this.setState({parsedResponse: cachedData})
-        //}.bind(this), 1000) // let you see the spinner
+        console.log("FORCE GRAPH DID MOUNT")
+
+
     }
 
 }
