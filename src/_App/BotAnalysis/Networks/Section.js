1c1190da66b864498a2136ba43db3c186343eeb0
MJ Rossetti <s2t2@users.noreply.github.com>
2020-10-30 16:01:07 -0400

    Let me see these on the same page while prototyping

--- src/_App/BotAnalysis/Networks/Section.js
+++ src/_App/BotAnalysis/Networks/Section.js
@@ -7,6 +7,7 @@ import Card from 'react-bootstrap/Card'
 //import Table from 'react-bootstrap/Table'
 
 import NetworkGraph from "./Graph"
+import TreeGraph from "./Graph1"
 
 export default function BotNetworks() {
     return (
@@ -15,7 +16,15 @@ export default function BotNetworks() {
                 <Card.Body>
                     <Card.Title><h3>Bot Networks</h3></Card.Title>
 
+                    <Card.Text>
+                        Bot Community Networks:
+                    </Card.Text>
                     <NetworkGraph/>
+
+                    <Card.Text>
+                        Prototype tree graph structure with levels:
+                    </Card.Text>
+                    <TreeGraph/>
                 </Card.Body>
             </Card>
         </Container>
