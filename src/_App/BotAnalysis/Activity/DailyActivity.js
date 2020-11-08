2af2b9c7a9aaf741fafcd089e40a9ee2ca5cda31
MJ Rossetti <s2t2@users.noreply.github.com>
2020-11-08 14:04:48 -0500

    Refactor metric into state

--- src/_App/BotAnalysis/Activity/DailyActivity.js
+++ src/_App/BotAnalysis/Activity/DailyActivity.js
@@ -11,17 +11,26 @@ import {opinionShiftScale as colorScale} from '../../Utils/Colors'
 import Spinner from '../../Spinner'
 import cachedData from '../../../data/bot_impact/assess_all_days.js' //'./data'
 
-export default class DailyOpinionShift extends PureComponent {
+const METRICS = {
+  "mean_opinion_shift": {
+        "chartTitle": "Daily Bot-Induced Opinion Shift",
+        "yAxisLabel": "Mean Pro-Trump Opinion Shift", "yAxisDomain": [-0.15, 0.09]
+    }
+}
+
+export default class DailyActivity extends PureComponent {
     constructor(props) {
         super(props)
-        this.state = {parsedResponse: null}
+        this.state = {metric: "mean_opinion_shift",parsedResponse: null}
         this.barFill = this.barFill.bind(this)
+        this.tooltipFormatter = this.tooltipFormatter.bind(this)
     }
 
     render() {
         var spinIntoChart = <Spinner/>
 
         if(this.state.parsedResponse){
+            var metric = this.state.metric
             var data = this.state.parsedResponse
             //console.log("DAILY OPINION SHIFT DATA", data)
 
@@ -31,9 +40,10 @@ export default class DailyOpinionShift extends PureComponent {
             })
             //console.log("DAILY OPINION SHIFT DATA 2", data)
 
-            const chartTitle = `Daily Bot-Induced Opinion Shift`
+            const chartTitle = METRICS[metric]["chartTitle"]
             const chartSubtitle = `Opinion Model: BERT Transformer`
-            const yAxisDomain = [-0.15, 0.09]
+            const yAxisDomain = METRICS[metric]["yAxisDomain"]
+            const yAxisLabel = METRICS[metric]["yAxisLabel"]
 
             spinIntoChart = (
                 <span>
@@ -52,8 +62,8 @@ export default class DailyOpinionShift extends PureComponent {
                     <div style={{width: "100%", height: 500}}>
                         <ResponsiveContainer>
                             <BarChart data={data} layout="horizontal" margin={{top: 0, bottom: 20, left: 5, right: 30}} barCategoryGap={0}>
-                                <YAxis type="number" dataKey="mean_opinion_shift" domain={yAxisDomain}>
-                                    <Label value="Mean Pro-Trump Opinion Shift" position="insideLeft" angle={-90} offset={0} style={{textAnchor: 'middle'}}/>
+                                <YAxis type="number" dataKey={metric} domain={yAxisDomain}>
+                                    <Label value={yAxisLabel} position="insideLeft" angle={-90} offset={0} style={{textAnchor: 'middle'}}/>
                                 </YAxis>
                                 <XAxis type="category" dataKey="date" tick={{fontSize: 14}}>
                                     <Label value="Date" position="insideBottom" offset={-15}/>
@@ -67,10 +77,10 @@ export default class DailyOpinionShift extends PureComponent {
                                     labelFormatter={this.tooltipLabelFormatter}
                                     formatter={this.tooltipFormatter}
                                 />
-                                <Bar dataKey="mean_opinion_shift" fill="#ccc" onClick={this.handleBarClick}>
+                                <Bar dataKey={metric} fill="#ccc" onClick={this.handleBarClick}>
                                     {
                                         data.map((entry, index) => (
-                                            <Cell key={entry["date"]} fill={this.barFill(entry["mean_opinion_shift"])}/>
+                                            <Cell key={entry["date"]} fill={this.barFill(entry[metric])}/>
                                         ))
                                     }
                                 </Bar>
@@ -113,7 +123,8 @@ export default class DailyOpinionShift extends PureComponent {
 
     tooltipFormatter(value, name, props){
         //console.log("FORMATTER", value, name, props)
-        return [formatPct(value), "Mean Pro-Trump Opinion Shift"]
+        const metric = this.state.metric
+        return [formatPct(value), METRICS[metric]["yAxisLabel"]]
     }
 
 }
