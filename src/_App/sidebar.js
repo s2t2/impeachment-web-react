51f0fda785a4c5db7c640e3ab03b78d12bd24355
MJ Rossetti <s2t2@users.noreply.github.com>
2020-12-10 12:23:31 -0500

    Add the networks page back in

--- src/_App/sidebar.js
+++ src/_App/sidebar.js
@@ -8,8 +8,7 @@ import CollectionResults from './TweetCollection/Results/Section'
 
 import BotAnalysis from './BotAnalysis/Page' // SectionIndex
 import BotDetection from './BotAnalysis/Detection/Section'
-//import BotNetworks from "./BotAnalysis/Networks/Section"
-//import BotActivity from './BotAnalysis/Activity/Section'
+import BotNetworks from "./BotAnalysis/Networks/Section"
 import BotBeneficiaries from './BotAnalysis/Beneficiaries/Section'
 import BotLanguage from './BotAnalysis/Language/Section'
 import BotImpact from './BotAnalysis/Impact/Section'
@@ -29,7 +28,7 @@ var sidebar = [
             {"key": "bot-beneficiaries",    "title": "Bot Beneficiaries",   "component": BotBeneficiaries},
             {"key": "bot-language",         "title": "Bot Language",        "component": BotLanguage},
             //{"key": "bot-activity",         "title": "Bot Activity",        "component": BotActivity},
-            //{"key": "bot-networks",         "title": "Bot Networks",        "component": BotNetworks},
+            {"key": "bot-networks",         "title": "Bot Networks",        "component": BotNetworks},
             {"key": "bot-detection",        "title": "Bot Detection",      "component": BotDetection}
         ]
     },
