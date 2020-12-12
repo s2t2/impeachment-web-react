4242152542f33b22e3561d8718cbdada65bce395
MJ Rossetti <s2t2@users.noreply.github.com>
2020-12-11 19:12:33 -0500

    Loading from CSV is very slow

--- src/_App/BotAnalysis/Followers/NOTES.md
+++ src/_App/BotAnalysis/Followers/NOTES.md
@@ -73,3 +73,118 @@ WHERE u.is_bot=True
 ```
 
 Save query results as JSON to Drive, and rename as "BotFollowerLinks.json". Rows: 29,843,033. Size: 2GB. Hmm that is very big. Will it fit within the Heroku slug size limit?
+
+How about two smaller graphs?
+
+## Graph 0
+
+
+```sql
+WITH nodes0 as (
+  SELECT DISTINCT node_id
+  FROM (
+    -- BOTS:
+    SELECT DISTINCT user_id as node_id
+    FROM impeachment_production.user_details_v6_slim u
+    WHERE u.is_bot=True and u.opinion_community=0
+
+    UNION ALL
+
+    -- BOT FOLLOWERS:
+    SELECT DISTINCT auff.follower_id as node_id
+    FROM impeachment_production.active_followers_flat_v2 auff
+    JOIN impeachment_production.user_details_v6_slim u ON u.user_id = auff.user_id
+    WHERE u.is_bot=True and u.opinion_community=0
+  )
+)
+
+--SELECT count(distinct node_id) as node_count
+--FROM nodes0 -- 504,883
+
+SELECT DISTINCT
+  user_id
+  --,created_on
+  ,screen_name_count
+  ,screen_names
+  ,is_bot
+  --,is_q
+  ,status_count
+  --,rt_count
+  --,opinion_community
+  ,coalesce(avg_score_bert, avg_score_nb, avg_score_lr) as mean_opinion
+  ,follower_count ,friend_count
+FROM nodes0
+JOIN impeachment_production.user_details_v6_slim u ON u.user_id = nodes0.node_id
+```
+
+Save query results as JSON to Drive, and rename as "Bot0FollowerNodes.json".
+
+```sql
+-- BOT FOLLOWERS:
+SELECT DISTINCT auff.user_id ,auff.follower_id
+FROM impeachment_production.active_followers_flat_v2 auff
+JOIN impeachment_production.user_details_v6_slim u ON u.user_id = auff.user_id
+WHERE u.is_bot=True and u.opinion_community = 0
+```
+
+Save query results as JSON to Drive, and rename as "Bot0FollowerLinks.json". Rows: 7,701,607. Size: 418 MB. Not terrible.
+
+## Graph1
+
+```sql
+WITH nodes1 as (
+  SELECT DISTINCT node_id
+  FROM (
+    -- BOTS:
+    SELECT DISTINCT user_id as node_id
+    FROM impeachment_production.user_details_v6_slim u
+    WHERE u.is_bot=True and u.opinion_community=1
+
+    UNION ALL
+
+    -- BOT FOLLOWERS:
+    SELECT DISTINCT auff.follower_id as node_id
+    FROM impeachment_production.active_followers_flat_v2 auff
+    JOIN impeachment_production.user_details_v6_slim u ON u.user_id = auff.user_id
+    WHERE u.is_bot=True and u.opinion_community=1
+  )
+)
+
+
+--SELECT count(distinct node_id) as node_count
+--FROM nodes1 -- 439,554
+
+
+SELECT DISTINCT
+  user_id
+  --,created_on
+  ,screen_name_count
+  ,screen_names
+  ,is_bot
+  --,is_q
+  ,status_count
+  --,rt_count
+  --,opinion_community
+  ,coalesce(avg_score_bert, avg_score_nb, avg_score_lr) as mean_opinion
+  ,follower_count ,friend_count
+FROM nodes1
+JOIN impeachment_production.user_details_v6_slim u ON u.user_id = nodes1.node_id
+
+```
+
+Save query results as JSON to Drive, and rename as "Bot1FollowerNodes.json". 76 MB.
+
+```sql
+-- BOT FOLLOWERS:
+SELECT DISTINCT auff.user_id ,auff.follower_id
+FROM impeachment_production.active_followers_flat_v2 auff
+JOIN impeachment_production.user_details_v6_slim u ON u.user_id = auff.user_id
+WHERE u.is_bot=True and u.opinion_community = 1
+```
+
+Save query results as JSON to Drive, and rename as "Bot1FollowerLinks.json". Rows: 22,141,426. Size: 1 GB. Maybe doable.
+
+...
+
+
+OK so it turns out BigQuery exports bad JSON. Not helpful. So let's write a custom python script instead... Or maybe we can export CSV and use another tool / service to convert CSV to JSON...
--- src/_App/BotAnalysis/Followers/Section.js
+++ src/_App/BotAnalysis/Followers/Section.js
@@ -1,52 +1,102 @@
 
-import React from 'react'
+import React, {PureComponent} from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
 
-import {legendBlue, legendRed} from "../../Utils/Colors"
+//import {legendBlue, legendRed} from "../../Utils/Colors"
 
-import Graph from "./Graph" // "./ExampleSizemeGraph"
-import {data0, data1} from "./ExampleData.js"
+//import Graph from "./Graph" // "./ExampleSizemeGraph"
+//import {data0, data1} from "./ExampleData.js"
 
-export default function BotFollowersSection() {
+import * as d3 from 'd3'
 
-    return (
+import Bot0FollowerNodes from './Bot0FollowerNodes.csv'
+import Bot0FollowerLinks from './Bot0FollowerLinks.csv'
+// user_id, screen_name_count, screen_names, is_bot, status_count, mean_opinion, follower_count, friend_count
 
-        <Container fluid>
-            <Card>
-                <Card.Body>
-                    <Card.Title><h3>Bot Follower Networks</h3></Card.Title>
 
-                    <Card.Text>
-                        The graphs below present the network of users who follow bots in each <a href="/bot-opinions">bot opinion community</a>.
-                    </Card.Text>
+export default class BotFollowersSection extends PureComponent {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
 
-                    <Row>
-                        <Col sm={12} md={12} lg={6}>
-                            <Card>
-                                <Card.Body>
-                                    <Card.Text className="app-center">Anti-Trump Bot Followers</Card.Text>
+    render() {
+        return (
+            <Container fluid>
+                <Card>
+                    <Card.Body>
+                        <Card.Title><h3>Bot Follower Networks</h3></Card.Title>
 
-                                    <Graph key="network-0" data={data0} nodeColor={legendBlue}/>
-                                </Card.Body>
-                            </Card>
-                        </Col>
+                        <Card.Text>
+                            The graphs below present the network of users who follow bots in each <a href="/bot-opinions">bot opinion community</a>.
+                        </Card.Text>
 
-                        <Col sm={12} md={12} lg={6}>
-                            <Card>
-                                <Card.Body>
-                                    <Card.Text className="app-center">Pro-Trump Bot Followers</Card.Text>
+                        <Row>
+                            <Col sm={12} md={12} lg={6}>
+                                <Card>
+                                    <Card.Body>
+                                        <Card.Text className="app-center">Anti-Trump Bot Followers</Card.Text>
+                                        {/*
+                                        <Graph key="network-0" data={data0} nodeColor={legendBlue}/>
+                                        */}
+                                    </Card.Body>
+                                </Card>
+                            </Col>
 
-                                    <Graph key="network-1" data={data1} nodeColor={legendRed}/>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    </Row>
+                            <Col sm={12} md={12} lg={6}>
+                                <Card>
+                                    <Card.Body>
+                                        <Card.Text className="app-center">Pro-Trump Bot Followers</Card.Text>
+                                        {/*
+                                        <Graph key="network-1" data={data1} nodeColor={legendRed}/>
+                                        */}
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        </Row>
 
-                </Card.Body>
-            </Card>
-        </Container>
-    )
+                    </Card.Body>
+                </Card>
+            </Container>
+        )
+    }
+
+    componentDidMount(){
+        console.log("COMPONENT DID MOUNT")
+
+        //d3.csv(Bot0FollowerNodes, function(nodes0) {
+        //    console.log("NODES-0:", nodes0.length, nodes0[0])
+        //    this.setState({nodes0: nodes0})
+        //}.bind(this))
+
+        //d3.csv(Bot0FollowerNodes).then(function(data) {
+        //    console.log("NODES-0:", data.length, data[0])
+        //    this.setState({nodes0: data})
+        //}.bind(this))
+
+        //d3.csv(Bot0FollowerLinks).then(function(data) {
+        //    console.log("LINKS-0:", data.length, data[0])
+        //    this.setState({links0: data})
+        //}.bind(this))
+
+
+        Promise.all([
+            d3.csv(Bot0FollowerNodes),
+            d3.csv(Bot0FollowerLinks),
+        ]).then(function(files) {
+            // files[0] will contain Bot0FollowerNodes
+            // files[1] will contain Bot0FollowerLinks
+            console.log(files[0].length)
+            console.log(files[1].length)
+        }).catch(function(err) {
+            console.log("ERROR LOADING THE DATA :-(")
+            console.log(err)
+        })
+
+
+    }
+
 }
