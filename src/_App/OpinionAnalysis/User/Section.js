d94f9ce34e222c5c4c32c056ccbb173addff6f1b
MJ Rossetti <s2t2@users.noreply.github.com>
2020-12-13 17:24:58 -0500

    Fully working typeahead

--- src/_App/OpinionAnalysis/User/Section.js
+++ src/_App/OpinionAnalysis/User/Section.js
@@ -2,88 +2,19 @@
 import React, { PureComponent } from 'react'
 import Container from 'react-bootstrap/Container'
 //import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+//import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
 //import Table from 'react-bootstrap/Table'
 import Form from 'react-bootstrap/Form'
 //import FormControl from 'react-bootstrap/FormControl'
 //import InputGroup from 'react-bootstrap/InputGroup'
-import Button from 'react-bootstrap/Button'
+//import Button from 'react-bootstrap/Button'
 import { Typeahead } from 'react-bootstrap-typeahead'
 import queryString from 'query-string'
 import ReactGA from 'react-ga'
 
 import UserOpinionDashboard from "./Dashboard"
-
 import users from "../TopUsers/data.js"
-
-//const exampleOptions = [
-//  { name: 'Alabama', population: 4780127, capital: 'Montgomery', region: 'South' },
-//  { name: 'Alaska', population: 710249, capital: 'Juneau', region: 'West' },
-//  { name: 'Arizona', population: 6392307, capital: 'Phoenix', region: 'West' },
-//]
-
-class MyTypeAhead extends PureComponent {
-
-    constructor(props) {
-        super(props)
-        var screenName = props["screen_name"]  || "Texas" //|| "SENATEMAJLDR"
-        this.state = {screenName: screenName}
-        this.handleSelection = this.handleSelection.bind(this)
-    }
-
-    handleSelection(selectedUsers){
-        //event.preventDefault() // prevent page reload
-        //var screenName = document.getElementById("inputScreenName").value
-
-        var screenName
-        var selectedUser = selectedUsers[0]
-        if(selectedUser){
-            screenName = selectedUser["screen_name"]
-            console.log("YOU SELECTED A SCREEN NAME!", screenName)
-            //ReactGA.event({
-            //    category: "User Chart Interaction",
-            //    action: "Input Screen Name",
-            //    value: screenName
-            //})
-        } else {
-            screenName = "" // prevent backspace key from doing funky things
-        }
-
-        this.setState({screenName: screenName})
-    }
-
-    render() {
-        return (
-            <Form.Group>
-                <Form.Label>Change User:</Form.Label>
-                <Typeahead
-                    id="top-users-typeahead"
-                    labelKey="screen_name"
-                    onChange={this.handleSelection}
-                    options={users}
-                    placeholder="SENATEMAJLDR"
-                    //selected={[this.state.screenName]}
-                />
-            </Form.Group>
-        )
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 export default class UserOpinionSection extends PureComponent {
@@ -93,18 +24,36 @@ export default class UserOpinionSection extends PureComponent {
         let params = queryString.parse(window.location.search)
         var screenName = props["screen_name"] || params["sn"] || "SENATEMAJLDR" // append ?sn=BERNIESANDERS to the URL to customize via URL params!!!
         this.state = {screenName: screenName}
-        this.handleSubmit = this.handleSubmit.bind(this)
+        //this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleSelection = this.handleSelection.bind(this)
     }
 
-    handleSubmit(event){
-        event.preventDefault() // prevent page reload
-        var screenName = document.getElementById("inputScreenName").value
-        console.log("YOU CLICKED ME!", screenName)
-        ReactGA.event({
-            category: "User Chart Interaction",
-            action: "Input Screen Name",
-            value: screenName
-        })
+    //handleSubmit(event){
+    //    event.preventDefault() // prevent page reload
+    //    var screenName = document.getElementById("inputScreenName").value
+    //    console.log("YOU CLICKED ME!", screenName)
+    //    ReactGA.event({
+    //        category: "User Chart Interaction",
+    //        action: "Submit Screen Name",
+    //        label: screenName
+    //    })
+    //    this.setState({screenName: screenName})
+    //}
+
+    handleSelection(selectedUsers){
+        var selectedUser = selectedUsers[0]
+        var screenName
+        if(selectedUser){
+            screenName = selectedUser["screen_name"]
+            console.log("YOU SELECTED A SCREEN NAME!", screenName)
+            ReactGA.event({
+                category: "User Chart Interaction",
+                action: "Select Screen Name",
+                label: screenName
+            })
+        } else {
+            screenName = "" // prevent backspace key from doing funky things
+        }
         this.setState({screenName: screenName})
     }
 
@@ -121,13 +70,13 @@ export default class UserOpinionSection extends PureComponent {
                             {" "} on the language patterns of the anti-Trump and pro-Trump bot retweet communities,
                             {" "} we used the models to predict Impeachment opinion scores for the remaining tweets in our dataset.
                             {" "}We then calculated the mean Impeachment opinion score for all users in our dataset.
-                        </Card.Text>
 
-                        <Card.Text>
-                            The dashboard below shows how our models scored the tweets of any given user (e.g. <i>REALDONALDTRUMP</i>, <i>SPEAKERPELOSI</i>, <i>SENATEMAJLDR</i>, <i>NYTIMES</i>, <i>WSJ</i>, etc.).
+
+                            {" "} The dashboard below shows Impeachment opinion scores for any given user.
                             {" "} NOTE: tweets by <i>FOXNEWS</i> contain only URLs, and are not scored.
                         </Card.Text>
 
+                        {/*
                         <Form style={{marginBottom:10}}>
                             <Form.Row>
                                 <Form.Label>Change User:</Form.Label>
@@ -145,9 +94,19 @@ export default class UserOpinionSection extends PureComponent {
 
                             </Form.Row>
                         </Form>
+                        */}
 
-                        <MyTypeAhead/>
-
+                        <Form.Group>
+                            <Form.Label>Change User:</Form.Label>
+                            <Typeahead
+                                id="top-users-typeahead"
+                                labelKey="screen_name"
+                                onChange={this.handleSelection}
+                                options={users}
+                                placeholder="SENATEMAJLDR"
+                                //selected={[this.state.screenName]}
+                            />
+                        </Form.Group>
 
                     </Card.Body>
                 </Card>
@@ -155,10 +114,14 @@ export default class UserOpinionSection extends PureComponent {
                 <Card>
                     <Card.Body>
                         {/*
-                        <UserOpinionDashboard key={screenName} screenName={screenName}/>
-                        */}
 
                         {screenName}
+
+                        */}
+
+                        <UserOpinionDashboard key={screenName} screenName={screenName}/>
+
+
                     </Card.Body>
                 </Card>
             </Container>
